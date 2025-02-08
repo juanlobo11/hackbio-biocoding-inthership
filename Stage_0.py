b612a922172c6a4c2b855163b81918786b0e6991
juanlobo11 <lobojuancamilo1@gmail.com>
2025-02-08 16:44:33 -0500

    Add files via upload

---
+++ Stage_0.py
@@ -0,0 +1,18 @@
+group_members_data = {'Member 1': {
+    "Name": "Juan Lobo",
+    "Email": "lobojuancamilo1@gmail.com",
+    "Slack username" : "juanlobo_11",
+    "Hobby" : "videogames",
+    "Country": "Colombia",
+    "Discipline": "Bioinformatics",
+    "Programming Language" : "Python"}}
+
+print(f'''
+Member 1:
+Name: {group_members_data["Member 1"]["Name"]}
+Email: {group_members_data["Member 1"]["Email"]}
+Slack username: {group_members_data["Member 1"]["Slack username"]}
+Hobby: {group_members_data["Member 1"]["Hobby"]}
+Country: {group_members_data["Member 1"]["Country"]}
+Discipline: {group_members_data["Member 1"]["Discipline"]}
+Programming Language: {group_members_data["Member 1"]["Programming Language"]}''')
